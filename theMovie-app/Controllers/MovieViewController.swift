abccc847de8f466e8bdf093e8e5a98d9488f3956
Adriel Alves <adrielfreitasalves@gmail.com>
2019-12-16 20:55:58 -0300

    adicionando textField e placeHolder

---
+++ theMovie-app/Controllers/MovieViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MovieViewController.swift
+//  theMovie-app
+//
+//  Created by Adriel Alves on 16/12/19.
+//  Copyright © 2019 adriel. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+    @IBOutlet weak var lbSearch: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ theMovie-app/Extensions/Extensions.swift
@@ -0,0 +1,10 @@
+//
+//  Extensions.swift
+//  theMovie-app
+//
+//  Created by Adriel Alves on 16/12/19.
+//  Copyright © 2019 adriel. All rights reserved.
+//
+
+import UIKit
+
